5e47ea2a473832a77a915ad84decc52235b347a6
task4233 <29667656+task4233@users.noreply.github.com>
2023-08-13 20:47:20 +0900

    add(api): logAdapter

--- api/authorize.go
+++ api/authorize.go
@@ -19,10 +19,31 @@ func NewAuthorizationServer(port int) Server {
 	return s
 }
 
-func (a *authorizationServer) Run() error {
+func (s *authorizationServer) Run() error {
 	panic("implement me")
 }
 
-func (a *authorizationServer) route() http.Handler {
+func (s *authorizationServer) route() http.Handler {
+	h := http.NewServeMux()
+
+	h.Handle("/authorize", logAdapter(http.HandlerFunc(s.authorize)))
+	h.Handle("/authenticate", logAdapter(http.HandlerFunc(s.authenticate)))
+	h.Handle("/token", logAdapter(http.HandlerFunc(s.token)))
+
+	return h
+}
+
+// authorize is for handing 2.send the authorization.
+func (s *authorizationServer) authorize(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+// authenticate is for handling 6.send the information for user authentication.
+func (s *authorizationServer) authenticate(w http.ResponseWriter, r *http.Request) {
+	panic("implement me")
+}
+
+// authenticate is for handling 8.send a token issue request.
+func (s *authorizationServer) token(w http.ResponseWriter, r *http.Request) {
 	panic("implement me")
 }
--- api/server.go
+++ api/server.go
@@ -1,5 +1,23 @@
 package api
 
+import (
+	"net/http"
+
+	"github.com/task4233/oauth/logger"
+)
+
 type Server interface {
 	Run() error
+}
+
+// logAdapter is a middleware for common logging for handlers.
+func logAdapter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := logger.FromContext(r.Context())
+		log.Info("[Req] %s %s\n", r.Method, r.URL.Path)
+
+		next.ServeHTTP(w, r)
+
+		log.Info("[Res] %s %s\n", r.Method, r.URL.Path)
+	})
 }
