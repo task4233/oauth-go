9ddce84ce88b763ac679daa278e85b3b5ad03a7c
task4233 <29667656+task4233@users.noreply.github.com>
2023-10-14 21:44:45 +0900

    change(api): add resource

--- api/resource.go
+++ api/resource.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/task4233/oauth/infra/repository"
+	"github.com/task4233/oauth/logger"
+	"golang.org/x/exp/slog"
 )
 
 // resourceServer is a server for hosting protected resources, capable of accepting
@@ -13,12 +16,16 @@ import (
 type resourceServer struct {
 	port int
 	srv  http.Server
+	kvs  repository.KVS
+	log  *slog.Logger
 }
 
 func NewResourceServer(ctx context.Context, port int, kvs repository.KVS) Server {
 	s := &resourceServer{port: port}
 	s.srv.Addr = fmt.Sprintf(":%d", port)
 	s.srv.Handler = s.route()
+	s.kvs = kvs
+	s.log = logger.FromContext(ctx)
 	return s
 }
 
@@ -26,8 +33,29 @@ func (s *resourceServer) Run() error {
 	return s.srv.ListenAndServe()
 }
 
-func (a *resourceServer) route() http.Handler {
+func (s *resourceServer) route() http.Handler {
 	h := http.NewServeMux()
-
+	h.HandleFunc("/", s.index)
 	return h
+}
+
+func (s *resourceServer) index(w http.ResponseWriter, r *http.Request) {
+	// extract bearer token from Authorization header
+	authHeader := strings.ToLower(r.Header.Get("Authorization"))
+	inToken, ok := strings.CutPrefix(authHeader, "bearer ")
+	if !ok {
+		s.log.Error("/index", "msg", "failed to extract bearer token from Authorization header", "Authorization header", authHeader)
+		http.Error(w, fmt.Errorf("failed to extract bearer token from Authorization header: %s", authHeader).Error(), http.StatusBadRequest)
+		return
+	}
+
+	// extract token from kvs
+	_, err := s.kvs.Get(inToken)
+	if err != nil {
+		s.log.Error("/index", "msg", "failed to extract token from kvs", "error", err)
+		http.Error(w, fmt.Errorf("failed to extract token from kvs: %w", err).Error(), http.StatusUnauthorized)
+		return
+	}
+
+	w.Write([]byte("You're authorized!"))
 }
