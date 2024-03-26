33cdee43d012355088cade2cc44e1d2f1431ca87
task4233 <29667656+task4233@users.noreply.github.com>
2024-03-26 21:12:42 +0900

    change(cmd/script): fix script for authorization code flow

--- cmd/script/authorization_code.sh
+++ cmd/script/authorization_code.sh
@@ -1,8 +1,8 @@
 curl \
     -H "Authorization: Bearer dummy_token:1" \
-    "http://localhost:8000/login"
+    "http://localhost:8000/authorize"
 
 curl \
     -H "Authorization: Bearer dummy_token:1" \
     -v \
-    "http://localhost:8000/login"
+    "http://localhost:8000/authorize"
